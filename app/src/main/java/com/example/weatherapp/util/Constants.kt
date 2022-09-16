84c2eea166797f075d3227b229c08fee80f833ea
braop <kazibwebrianpeter@gmail.com>
2022-09-16 23:42:27 +0300

    refixing lat long

--- app/src/main/java/com/example/weatherapp/util/Constants.kt
+++ app/src/main/java/com/example/weatherapp/util/Constants.kt
@@ -3,7 +3,7 @@ package com.example.weatherapp.util
 class Constants {
     companion object {
         const val BASE_URL = "http://api.openweathermap.org/data/2.5/"
-        const val APP_ID = "63f6a732652e0076ef7243052596bcb6"
+        const val APP_ID = ""
         const val UNITS = "metric"
         const val CLOUDS = "Clouds"
         const val RAIN = "Rain"
