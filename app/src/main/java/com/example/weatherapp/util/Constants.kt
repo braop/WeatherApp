4f911e797b945b6e62a2535ca0a44412da7ef9ef
braop <kazibwebrianpeter@gmail.com>
2022-09-19 15:48:11 +0300

    deleting un-used resource files

--- app/src/main/java/com/example/weatherapp/util/Constants.kt
+++ app/src/main/java/com/example/weatherapp/util/Constants.kt
@@ -3,8 +3,8 @@ package com.example.weatherapp.util
 class Constants {
     companion object {
         const val BASE_URL = "http://api.openweathermap.org/data/2.5/"
-        const val APP_ID = "780da7b7299d5e95b8c08e44579d3aba"
-        const val PLACES_API = "AIzaSyDPO9-rY_LqgEUx0OtWT1Uh51Fjaz9bIMQ"
+        const val APP_ID = "xxxxxxxxxxxxxxxxxx"
+        const val PLACES_API = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
         const val UNITS = "metric"
         const val CLOUDS = "Clouds"
         const val RAIN = "Rain"
