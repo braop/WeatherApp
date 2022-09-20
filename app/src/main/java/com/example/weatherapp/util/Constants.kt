c496b1e421deae01eef79b0fccf4be97a60205e0
braop <kazibwebrianpeter@gmail.com>
2022-09-20 08:22:19 +0300

    adding key and ID

--- app/src/main/java/com/example/weatherapp/util/Constants.kt
+++ app/src/main/java/com/example/weatherapp/util/Constants.kt
@@ -3,8 +3,8 @@ package com.example.weatherapp.util
 class Constants {
     companion object {
         const val BASE_URL = "http://api.openweathermap.org/data/2.5/"
-        const val APP_ID = "XXXXXXXXXXXXXXXXXXXX"
-        const val PLACES_API = "XXXXXXXXXXXXXXXXXXXXXX"
+        const val APP_ID = "b7869283e6590fcc24997f1f40b00b65"
+        const val PLACES_API = "AIzaSyDPO9-rY_LqgEUx0OtWT1Uh51Fjaz9bIMQ"
         const val UNITS = "metric"
         const val CLOUDS = "Clouds"
         const val RAIN = "Rain"
